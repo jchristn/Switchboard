c94b11d295597ed1b57621b4e6ec027f34a29b37
Joel Christner <joel.christner@gmail.com>
2024-12-04 19:57:50 -0800

    Docker

--- Docker/run.bat
+++ Docker/run.bat
@@ -2,32 +2,30 @@
 
 IF "%1" == "" GOTO :Usage
 
-if not exist litegraph.json (
-  echo Configuration file litegraph.json not found.
+if not exist sb.json (
+  echo Configuration file sb.json not found.
   exit /b 1
 )
 
 REM Items that require persistence
-REM   litegraph.json
-REM   litegraph.db
+REM   sb.json
 REM   logs/
 
 REM Argument order matters!
 
 docker run ^
-  -p 8701:8701 ^
+  -p 8000:8000 ^
   -t ^
   -i ^
   -e "TERM=xterm-256color" ^
-  -v .\litegraph.json:/app/litegraph.json ^
-  -v .\litegraph.db:/app/litegraph.db ^
+  -v .\sb.json:/app/sb.json ^
   -v .\logs\:/app/logs/ ^
-  jchristn/litegraph:%1
+  jchristn/switchboard:%1
 
 GOTO :Done
 
 :Usage
 ECHO Provide one argument indicating the tag. 
-ECHO Example: dockerrun.bat v2.0.17
+ECHO Example: dockerrun.bat v1.0.0
 :Done
 @echo on
--- Docker/run.sh
+++ Docker/run.sh
@@ -1,29 +1,27 @@
 if [ -z "${IMG_TAG}" ]; then
-  IMG_TAG='v2.0.17'
+  IMG_TAG='v1.0.0'
 fi
 
 echo Using image tag $IMG_TAG
 
-if [ ! -f "litegraph.json" ]
+if [ ! -f "sb.json" ]
 then
-  echo Configuration file litegraph.json not found.
+  echo Configuration file sb.json not found.
   exit
 fi
 
 # Items that require persistence
-#   litegraph.json
-#   litegraph.db
+#   sb.json
 #   logs/
 
 # Argument order matters!
 
 docker run \
-  -p 8701:8701 \
+  -p 8000:8000 \
   -t \
   -i \
   -e "TERM=xterm-256color" \
-  -v ./litegraph.json:/app/litegraph.json \
-  -v ./litegraph.db:/app/litegraph.db \
+  -v ./sb.json:/app/sb.json \
   -v ./logs/:/app/logs/ \
-  jchristn/litegraph:$IMG_TAG
+  jchristn/switchboard:$IMG_TAG
 
